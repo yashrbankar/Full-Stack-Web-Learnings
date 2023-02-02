8bf1c45c12bd108024e9b4bbd961cd00f982a241
yashrbankar <yashrbankar@gmail.com>
2023-02-02 23:20:39 +0530

    pushing all codes to github

---
+++ Web develpment/CSS Learning/html learning/test.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<pair<int, int>> v;
+    v.push_back({5,6});
+    v.push_back({8,7});
+    v.push_back({6,6});
+    v.push_back({9,8});
+    v.push_back({5,10});
+    v.push_back({1,8});
+    sort(v.begin(),v.end());
+    for()
+    {
+        
+    }
+
+}
