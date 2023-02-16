4c3e2234807ff0aa181f835b6421e68901050a38
yashrbankar <yashrbankar@gmail.com>
2023-02-16 16:24:46 +0530

    basic of js

---
+++ Anuj bhaiya/JS/learning.js
@@ -0,0 +1,60 @@
+// basic syntax in js
+
+//  this is commemt in js
+// varible types : let , var , const
+
+// let arr=['yash' ,1,"aniket", 2]
+// console.log(arr);
+// console.log("the size of array is :"+arr.length);
+// acessing
+// console.log("name :"+ arr[0]);
+// arr[6]="siddhesh"; 
+
+// arr.push("the")
+// console.log(arr);
+// arr.sort()
+// console.log(arr.indexOf('yash'));
+
+// for(let val in arr)
+//  {
+//    console.log("hello "+arr[val])
+//  }
+// arr.forEach(myFunction);
+
+// arr.slice(2,4); for deleting from index to n third parameter used for for replacing
+
+// console.log(basic(3,4))    
+// function basic(a,b) // no need of datatype of variables 
+// {
+//     console.log("this testing functions")
+//     console.log("this testing functions")
+//     return a+b;
+// }
+
+// Function Expression
+// let add;
+// error due to the use before the intailization
+// console.log("ans::"+add3(1,2));
+// console.log(typeof(add3))
+
+// add3=function(a,b){return a+b}
+// console.log("ans::"+add3(1,2));
+// console.log(typeof(add3))
+
+// let nums=[11,12,4,1,7,9]
+// nums.sort();
+// console.log(nums)
+
+// using compartor function
+// nums.sort((x,y)=>
+//          {
+//   return x-y;
+// return x>y
+// });
+// console.log(nums)
+
+
+
+
+
+
