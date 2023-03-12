a4578120c598506749c7fa1cd2b25e584b152d81
yashrbankar <yashrbankar@gmail.com>
2023-03-12 22:41:49 +0530

    changes 2

--- Anuj bhaiya/JS/DOM/navbar/script.js
+++ Anuj bhaiya/JS/DOM/navbar/script.js
@@ -1,8 +1,6 @@
 const toggler= document.getElementById("toggler");
 const navMenu=document.getElementById("nav-links");
 
-console.log(navMenu);
-console.log(toggler);
 toggler.addEventListener("click",()=>{
     navMenu.classList.toggle("active")
 });
